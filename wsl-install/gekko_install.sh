e226a3d58070d4c840f07b05cc889f23636a0a39
Tommie Hansen <tommiehansen@gmail.com>
2018-02-11 21:16:35 +0100

    update

--- wsl-install/gekko_install.sh
+++ wsl-install/gekko_install.sh
@@ -5,18 +5,18 @@ echo 'INSTALLING NODE + DEPENDECIES + GEKKO'
 echo '-------------------------------------------'
 echo 'This will take a while, go get some coffe'
 sleep 3
-#sudo su
-#apt-get update -y && apt-upgrade -y && apt-get update -y
-#curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-#apt-get install nodejs -y
-#apt-get install build-essential -y
-#apt-get install git -y
-#cd /mnt/c/
-#mkdir www
-#git clone git://github.com/askmike/gekko.git
-#cd gekko
-#npm install --only=production
-#npm install tulind
-#apt-get install nano -y && apt-get install jed -y
-#apt-get autoremove -y
-#node gekko --ui
+sudo su
+apt-get update -y && apt-upgrade -y && apt-get update -y
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install nodejs -y
+apt-get install build-essential -y
+apt-get install git -y
+cd /mnt/c/
+mkdir www
+git clone git://github.com/askmike/gekko.git
+cd gekko
+npm install --only=production
+npm install tulind
+apt-get install nano -y && apt-get install jed -y
+apt-get autoremove -y
+node gekko --ui
