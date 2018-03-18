98e1d8cabb10e89617a095657ee4941efe5f38a6
Tommie Hansen <tommiehansen@gmail.com>
2018-03-18 20:09:49 +0100

    Set this.debug to false again

--- strategies/RSI_BULL_BEAR_ADX.js
+++ strategies/RSI_BULL_BEAR_ADX.js
@@ -28,7 +28,7 @@ var strat = {
 		this.resetTrend();
 		
 		// debug? set to false to disable all logging/messages/stats (improves performance in backtests)
-		this.debug = true;
+		this.debug = false;
 		
 		// performance
 		config.backtest.batchSize = 1000; // increase performance
